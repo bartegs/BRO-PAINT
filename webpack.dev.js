fb0fc6163c31c5dbea7d00166fa08e2a63d5a6bf
bartegs <barteksadecki@gmail.com>
2021-05-26 20:22:40 +0200

    BRO-4 Set ts-compiler output and change webpack output path

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
 
   output: {
     filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "dist/client"),
   },
 
   devServer: {
--- webpack.prod.js
+++ webpack.prod.js
@@ -6,6 +6,6 @@ module.exports = merge(common, {
   mode: process.env.NODE_ENV || "production",
   output: {
     filename: "[name].[hash:5].js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "dist/client"),
   },
 });
