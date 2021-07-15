a17c2a22502c86de076ff0e0aa4abd9e91beef59
Aleksander Anonim <alexs98@wp.pl>
2021-07-15 21:54:57 +0200

    BRO-19 Created axis proto #1

--- apps/client/src/react/pages/RepairStatusPage/RepairStatusPage.tsx
+++ apps/client/src/react/pages/RepairStatusPage/RepairStatusPage.tsx
@@ -1,5 +1,57 @@
 import * as React from "react";
 
 export function RepairStatusPage(): JSX.Element {
-  return <div className="container">Miki</div>;
+  enum Color {
+    black = "#303030",
+    green = "#008377",
+    blue = "#006180",
+    pink = "#6F0032",
+  }
+
+  interface Stage {
+    title: string;
+    descrption: string;
+    color: Color;
+  }
+
+  type Stages = Stage[];
+
+  const stages: Stages = [
+    {
+      color: Color.black,
+      title: "Zlecenie przyjęte Zaczynamy!",
+      descrption: "test",
+    },
+    { color: Color.green, title: "Prace przygotowawcze", descrption: "test" },
+    { color: Color.blue, title: "Prace lakiernicze", descrption: "test" },
+    {
+      color: Color.pink,
+      title: "Detailing i kontrola jakości",
+      descrption: "test",
+    },
+    {
+      color: Color.pink,
+      title: "Zlecenie ukończone. Możesz odebrać auto!",
+      descrption: "test",
+    },
+  ];
+
+  return (
+    <div className="container repair-status-page">
+      {stages.map((item) => (
+        <div className="repair-status-page__segment">
+          <div
+            className="axis-fragment"
+            style={{ backgroundColor: item.color }}
+          >
+            <div
+              className="axis-fragment__point"
+              style={{ backgroundColor: item.color }}
+            />
+          </div>
+          <div className="axis-card">{item.title}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
