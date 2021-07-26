3050dd40f71d0170661bfe0c247ff336b5897fdd
Elfant <alexs98@wp.pl>
2021-07-26 23:17:39 +0200

    BRO-24 Add common margin and padding for content on every page.

--- apps/client/src/react/pages/HomePage/HomePage.tsx
+++ apps/client/src/react/pages/HomePage/HomePage.tsx
@@ -2,11 +2,17 @@ import * as React from "react";
 import { HomePageForm } from "./components/HomePageForm";
 
 export function HomePage(): JSX.Element {
+  React.useEffect(() => {
+    document.querySelector("#root").classList.add("root-background");
+
+    return () => {
+      document.querySelector("#root").classList.remove("root-background");
+    };
+  }, []);
+
   return (
     <div className="home-page">
-      <div className="home-page__content container">
-        <HomePageForm />
-      </div>
+      <HomePageForm />
     </div>
   );
 }
