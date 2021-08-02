0ab144f349a0558a179ebddc0500683c489d30f6
bartegs <57559914+bartegs@users.noreply.github.com>
2021-08-02 17:09:58 +0200

    BRO-25 Add full screen image preview

--- apps/client/src/react/pages/GalleryPage/components/Gallery.tsx
+++ apps/client/src/react/pages/GalleryPage/components/Gallery.tsx
@@ -1,18 +1,81 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import * as React from "react";
+import * as Modal from "react-modal";
 import { audi } from "../assets";
 
+Modal.setAppElement("div");
+
 export function Gallery(): JSX.Element {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
-    <div className="gallery-page__gallery">
-      <img className="gallery-page__gallery__image" src={audi} alt="" />
-      <img className="gallery-page__gallery__image" src={audi} alt="" />
-      <img className="gallery-page__gallery__image" src={audi} alt="" />
-      <img className="gallery-page__gallery__image" src={audi} alt="" />
-      <img className="gallery-page__gallery__image" src={audi} alt="" />
-      <img className="gallery-page__gallery__image" src={audi} alt="" />
-      <img className="gallery-page__gallery__image" src={audi} alt="" />
-      <img className="gallery-page__gallery__image" src={audi} alt="" />
-      <img className="gallery-page__gallery__image" src={audi} alt="" />
-    </div>
+    <>
+      <div id="gallery" className="gallery-page__gallery">
+        <img
+          onClick={openModal}
+          className="gallery-page__gallery__image"
+          src={audi}
+          alt=""
+        />
+        <img
+          onClick={openModal}
+          className="gallery-page__gallery__image"
+          src={audi}
+          alt=""
+        />
+        <img
+          onClick={openModal}
+          className="gallery-page__gallery__image"
+          src={audi}
+          alt=""
+        />
+        <img
+          onClick={openModal}
+          className="gallery-page__gallery__image"
+          src={audi}
+          alt=""
+        />
+        <img
+          onClick={openModal}
+          className="gallery-page__gallery__image"
+          src={audi}
+          alt=""
+        />
+        <img
+          onClick={openModal}
+          className="gallery-page__gallery__image"
+          src={audi}
+          alt=""
+        />
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        className="gallery-page__gallery__modal"
+        overlayClassName="gallery-page__gallery__overlay"
+      >
+        <img
+          className="gallery-page__gallery__modal__full-screen-image"
+          src={audi}
+          alt=""
+        />
+        <div className="gallery-page__gallery__modal__button__container">
+          <button
+            className="gallery-page__gallery__modal__button"
+            aria-label="Zamknij pełny ekran"
+            onClick={closeModal}
+          />
+        </div>
+      </Modal>
+    </>
   );
 }
