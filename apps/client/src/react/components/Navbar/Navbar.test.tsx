1aaee252c3f265580b277a5f837c1361313edc78
bartegs <57559914+bartegs@users.noreply.github.com>
2021-09-05 08:36:31 +0200

    BRO-41 Test Navbar, NavbarMenu and NavbarToggler

---
+++ apps/client/src/react/components/Navbar/Navbar.test.tsx
@@ -0,0 +1,59 @@
+import { render } from "@testing-library/react";
+import * as React from "react";
+import { MemoryRouter } from "react-router-dom";
+import { Navbar } from ".";
+
+describe("Navbar", () => {
+  it(`should render correctly to the page`, () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Navbar />
+      </MemoryRouter>
+    );
+
+    expect(container).toBeTruthy();
+  });
+
+  it(`should have "navbar" class`, () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Navbar />
+      </MemoryRouter>
+    );
+
+    expect(container.firstChild).toHaveClass("navbar");
+  });
+
+  it(`should render an anchor tag with a class "navbar__logo"`, () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Navbar />
+      </MemoryRouter>
+    );
+
+    const logo = container.querySelector("a.navbar__logo");
+    expect(logo).toBeInTheDocument();
+  });
+
+  it(`should render a ul tag with a list of menu options with a class "navbar-menu"`, () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Navbar />
+      </MemoryRouter>
+    );
+
+    const menu = container.querySelector("ul.navbar-menu");
+    expect(menu).toBeInTheDocument();
+  });
+
+  it(`should render a div tag with a class "navbar-toggler"`, () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Navbar />
+      </MemoryRouter>
+    );
+
+    const toggler = container.querySelector("div.navbar-toggler");
+    expect(toggler).toBeInTheDocument();
+  });
+});
---
+++ apps/client/src/react/components/Navbar/components/NavbarToggler.test.tsx
@@ -0,0 +1,101 @@
+import { render } from "@testing-library/react";
+import * as React from "react";
+import { MemoryRouter } from "react-router-dom";
+import { NavbarToggler } from ".";
+
+describe("NavbarMenu", () => {
+  // props mock
+  let isMenuOpen = false;
+  const handleMenuKeyboard = (e: React.KeyboardEvent) => {
+    if (e.code === "Enter" || e.code === "Space") {
+      isMenuOpen = !isMenuOpen;
+    } else if (e.code === "Escape") {
+      isMenuOpen = false;
+    }
+  };
+  const toggleMenu = (e: React.MouseEvent) => {
+    isMenuOpen = !isMenuOpen;
+    e.stopPropagation();
+  };
+
+  it(`should render correctly to the page`, () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <NavbarToggler
+          onClick={toggleMenu}
+          isClicked={isMenuOpen}
+          handleMenuKeyboard={handleMenuKeyboard}
+        />
+      </MemoryRouter>
+    );
+
+    expect(container).toBeTruthy();
+  });
+
+  it(`should render a div tag with a class "navbar-toggler"`, () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <NavbarToggler
+          onClick={toggleMenu}
+          isClicked={isMenuOpen}
+          handleMenuKeyboard={handleMenuKeyboard}
+        />
+      </MemoryRouter>
+    );
+
+    const toggler = container.querySelector("div.navbar-toggler");
+    expect(toggler).toBeInTheDocument();
+  });
+
+  it(`should have a div containg the toggler with a class "navbar-toggler--close" when isMenuOpen equals true`, () => {
+    isMenuOpen = true;
+
+    const { container } = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <NavbarToggler
+          onClick={toggleMenu}
+          isClicked={isMenuOpen}
+          handleMenuKeyboard={handleMenuKeyboard}
+        />
+      </MemoryRouter>
+    );
+
+    const closedToggler = container.querySelector("div.navbar-toggler--close");
+    expect(closedToggler).toBeInTheDocument();
+  });
+
+  it(`shouldn have a div containg the toggler with a class "navbar-toggler" but without "navbar-toggler--close" when isMenuOpen equals false`, () => {
+    isMenuOpen = false;
+
+    const { container } = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <NavbarToggler
+          onClick={toggleMenu}
+          isClicked={isMenuOpen}
+          handleMenuKeyboard={handleMenuKeyboard}
+        />
+      </MemoryRouter>
+    );
+
+    const closedToggler = container.querySelector("div.navbar-toggler--close");
+    expect(closedToggler).not.toBeInTheDocument();
+
+    const toggler = container.querySelector("div.navbar-toggler");
+    expect(toggler).toBeInTheDocument();
+  });
+
+  it(`should render a div tag with a class "navbar-toggler__bar"`, () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <NavbarToggler
+          onClick={toggleMenu}
+          isClicked={isMenuOpen}
+          handleMenuKeyboard={handleMenuKeyboard}
+        />
+      </MemoryRouter>
+    );
+
+    const togglerBar = container.querySelector("div.navbar-toggler__bar");
+    expect(togglerBar).toBeInTheDocument();
+  });
+});
