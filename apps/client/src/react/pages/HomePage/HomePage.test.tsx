d804b3f6378044b02eb638516b066ae73ca795e3
bartegs <57559914+bartegs@users.noreply.github.com>
2021-09-06 18:08:13 +0200

    BRO-28 Test HomePage

---
+++ apps/client/src/react/pages/HomePage/HomePage.test.tsx
@@ -0,0 +1,42 @@
+import { screen, render } from "@testing-library/react";
+import * as React from "react";
+import { HomePage } from ".";
+
+// walkaround for a useEffect console warning
+// eslint-disable-next-line no-console
+console.error = jest.fn();
+
+describe("HomePage", () => {
+  it(`should render correctly to the page`, () => {
+    const { container } = render(
+      <div id="root">
+        <HomePage />
+      </div>
+    );
+
+    const homePage = container.querySelector("div.home-page");
+    expect(homePage).toBeInTheDocument();
+  });
+
+  it(`should render a form for checking order status`, () => {
+    render(
+      <div id="root">
+        <HomePage />
+      </div>
+    );
+
+    const form = screen.getByText("Sprawdź status zlecenia");
+    expect(form).toBeInTheDocument();
+  });
+
+  it(`should have a "root-background" class which provides baner image`, () => {
+    const { container } = render(
+      <div id="root">
+        <HomePage />
+      </div>
+    );
+
+    const baner = container.querySelector("div.root-background");
+    expect(baner).toBeInTheDocument();
+  });
+});
