2e9b275020bf34f4970415bff173c2901f3c0183
bartegs <57559914+bartegs@users.noreply.github.com>
2021-09-14 16:51:18 +0200

    BRO-45 Test Checkbox

---
+++ apps/client/src/react/components/forms/components/Checkbox/Checkbox.test.tsx
@@ -0,0 +1,138 @@
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import * as React from "react";
+import { Checkbox } from ".";
+
+describe("NewRepairPage", () => {
+  let isChecked: boolean = true;
+  const setState = () => !isChecked;
+
+  it(`should render label`, () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={["/nowa-naprawa"]}>
+        <Checkbox
+          name="test"
+          id="test"
+          color="green"
+          isChecked={isChecked}
+          setState={setState}
+          required
+        />
+      </MemoryRouter>
+    );
+
+    const label = container.querySelector("label");
+    expect(label).toBeInTheDocument();
+  });
+
+  it(`should render checkbox input`, () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={["/nowa-naprawa"]}>
+        <Checkbox
+          name="test"
+          id="test"
+          color="green"
+          isChecked={isChecked}
+          setState={setState}
+          required
+        />
+      </MemoryRouter>
+    );
+
+    const checkbox = container.querySelector(`input[type="checkbox"]`);
+    expect(checkbox).toBeInTheDocument();
+  });
+
+  it(`should render checkbox input with a "chekbox" class`, () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={["/nowa-naprawa"]}>
+        <Checkbox
+          name="test"
+          id="test"
+          color="green"
+          isChecked={isChecked}
+          setState={setState}
+          required
+        />
+      </MemoryRouter>
+    );
+
+    const checkbox = container.querySelector(`input.checkbox`);
+    expect(checkbox).toBeInTheDocument();
+  });
+
+  it(`should render checkbox with the color given in props`, () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={["/nowa-naprawa"]}>
+        <Checkbox
+          name="test"
+          id="test"
+          color="green"
+          isChecked={isChecked}
+          setState={setState}
+          required
+        />
+      </MemoryRouter>
+    );
+
+    const checkboxColor = container.querySelector(`input.checkbox--green`);
+    expect(checkboxColor).toBeInTheDocument();
+  });
+
+  it(`Should render a description paragraph for checkbox`, () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={["/nowa-naprawa"]}>
+        <Checkbox
+          name="test"
+          id="test"
+          color="green"
+          isChecked={isChecked}
+          setState={setState}
+          required
+        />
+      </MemoryRouter>
+    );
+
+    const checkboxText = container.querySelector(
+      "p.new-repair-page__checkbox-paragraph"
+    );
+    expect(checkboxText).toBeInTheDocument();
+  });
+
+  it(`should be checked when isChecked prop equals true`, () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={["/nowa-naprawa"]}>
+        <Checkbox
+          name="test"
+          id="test"
+          color="green"
+          isChecked={isChecked}
+          setState={setState}
+          required
+        />
+      </MemoryRouter>
+    );
+
+    const checkboxChecked = container.querySelector(`input[checked]`);
+    expect(checkboxChecked).toBeTruthy();
+  });
+
+  it(`shouldn't be checked when isChecked prop equals false`, () => {
+    isChecked = false;
+    const { container } = render(
+      <MemoryRouter initialEntries={["/nowa-naprawa"]}>
+        <Checkbox
+          name="test"
+          id="test"
+          color="green"
+          isChecked={isChecked}
+          setState={setState}
+          required
+        />
+      </MemoryRouter>
+    );
+
+    const checkboxUnchecked = container.querySelector(`input[checked]`);
+    expect(checkboxUnchecked).toBeFalsy();
+  });
+});
