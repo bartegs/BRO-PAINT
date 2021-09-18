9b6a24381a5a1288cc3f740c4aa006eb7e453984
bartegs <57559914+bartegs@users.noreply.github.com>
2021-09-18 19:27:04 +0200

    BRO-45 Further test Button

--- apps/client/src/react/components/Button/Button.test.tsx
+++ apps/client/src/react/components/Button/Button.test.tsx
@@ -1,56 +1,77 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import * as React from "react";
-
-import { Color } from "../../../../../common/utils/types";
-
-import { Button, ButtonProps } from ".";
+import { Button } from ".";
 
 describe("Button", () => {
-  const defaultProps: ButtonProps = {
-    color: "green",
-    variant: "primary",
-    text: "sample label",
-  };
+  it(`should render button element`, () => {
+    const { container } = render(<Button text="click" color="green" />);
 
-  it("should render correctly class", () => {
-    // given
-    const color: Color = "green";
-
-    // when
-    const { container } = render(<Button {...defaultProps} color={color} />);
-
-    // then
-    expect(
-      (container.firstChild as Element).classList.contains(
-        "button--green-primary"
-      )
-    ).toBeTruthy();
+    const button = container.querySelector("button");
+    expect(button).toBeInTheDocument();
   });
 
-  it("should render correctly label", () => {
-    // given
-    const label = "Super Label";
+  it(`should have a "button" class`, () => {
+    const { container } = render(<Button text="click" color="green" />);
 
-    // when
-    render(<Button {...defaultProps} text={label} />);
-
-    // then
-    const button = screen.getByText(label);
-
-    expect(button).toBeTruthy();
+    const buttonClass = container.querySelector("button.button");
+    expect(buttonClass).toBeInTheDocument();
   });
 
-  it("should correctly use callback", () => {
-    // given
-    const callback = jest.fn();
+  it(`should correctly apply the variant given in the prop`, () => {
     const { container } = render(
-      <Button {...defaultProps} onClick={callback} />
+      <Button text="click" color="green" variant="primary" />
     );
 
-    // when
-    fireEvent.click(container.firstChild);
+    const buttonVariant = container.querySelector("button.button--primary");
+    expect(buttonVariant).toBeInTheDocument();
+  });
 
-    // then
+  it(`should correctly apply the color given in the prop`, () => {
+    const { container } = render(
+      <Button text="click" color="green" variant="primary" />
+    );
+
+    const buttonColor = container.querySelector("button.button--green-primary");
+    expect(buttonColor).toBeInTheDocument();
+  });
+
+  it(`should should accept additional css classes via props and properly apply it`, () => {
+    const { container } = render(
+      <Button additionalClasses="mt-2" text="click" color="green" />
+    );
+
+    const additionalClass = container.querySelector(`button.mt-2`);
+    expect(additionalClass).toBeInTheDocument();
+  });
+
+  it(`should should have a default type of button`, () => {
+    const { container } = render(<Button text="click" color="green" />);
+
+    const type = container.querySelector(`button[type="button"]`);
+    expect(type).toBeInTheDocument();
+  });
+
+  it(`should should accept button type via props and properly apply it`, () => {
+    const { container } = render(
+      <Button text="click" color="green" type="submit" />
+    );
+
+    const typeSubmit = container.querySelector(`button[type="submit"]`);
+    expect(typeSubmit).toBeInTheDocument();
+  });
+
+  it(`should render the button text given in the text prop`, () => {
+    render(<Button text="click" color="green" />);
+
+    const buttonText = screen.getByText(`click`);
+    expect(buttonText).toBeInTheDocument();
+  });
+
+  it("should call the callback function on button click", () => {
+    const callback = jest.fn();
+    render(<Button text="click" color="green" onClick={callback} />);
+
+    fireEvent.click(screen.getByText(`click`));
     expect(callback).toHaveBeenCalled();
   });
 });
