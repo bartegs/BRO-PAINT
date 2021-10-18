ec0e174103bddb73b789d6da46672707ed0c7b05
Elfant <alexs98@wp.pl>
2021-10-18 14:39:10 +0200

    BRO-54 Repair tests

--- apps/client/src/react/pages/OrderStatusPage/OrderStatusPage.test.tsx
+++ apps/client/src/react/pages/OrderStatusPage/OrderStatusPage.test.tsx
@@ -2,24 +2,37 @@ import { render, screen } from "@testing-library/react";
 import * as React from "react";
 
 import { OrderStatusPage } from "./OrderStatusPage";
+import ClientContextProvider from "../../contexts/ClientContext";
 
 describe("OrderStatusPage", () => {
   it(`should render correctly to the page`, () => {
-    const { container } = render(<OrderStatusPage />);
+    const { container } = render(
+      <ClientContextProvider>
+        <OrderStatusPage />)
+      </ClientContextProvider>
+    );
 
     const orderStatusPage = container.querySelector("div.order-status-page");
     expect(orderStatusPage).toBeInTheDocument();
   });
 
   it(`should render a form for checking order status`, () => {
-    render(<OrderStatusPage />);
+    render(
+      <ClientContextProvider>
+        <OrderStatusPage />)
+      </ClientContextProvider>
+    );
 
     const form = screen.getByText("Sprawdź status zlecenia");
     expect(form).toBeInTheDocument();
   });
 
   it(`should render axis`, () => {
-    const { container } = render(<OrderStatusPage />);
+    const { container } = render(
+      <ClientContextProvider>
+        <OrderStatusPage />)
+      </ClientContextProvider>
+    );
 
     const axis = container.querySelector(
       "section.order-status-page__axis-container"
@@ -28,35 +41,55 @@ describe("OrderStatusPage", () => {
   });
 
   it(`should render a first stage card with text "Zlecenie przyjęte Zaczynamy!"`, () => {
-    render(<OrderStatusPage />);
+    render(
+      <ClientContextProvider>
+        <OrderStatusPage />)
+      </ClientContextProvider>
+    );
 
     const firstStageCard = screen.getByText("Zlecenie przyjęte Zaczynamy!");
     expect(firstStageCard).toBeInTheDocument();
   });
 
   it(`should render a second stage card with text "Prace przygotowawcze"`, () => {
-    render(<OrderStatusPage />);
+    render(
+      <ClientContextProvider>
+        <OrderStatusPage />)
+      </ClientContextProvider>
+    );
 
     const secondStageCard = screen.getByText("Prace przygotowawcze");
     expect(secondStageCard).toBeInTheDocument();
   });
 
   it(`should render a third stage card with text "Prace lakiernicze"`, () => {
-    render(<OrderStatusPage />);
+    render(
+      <ClientContextProvider>
+        <OrderStatusPage />)
+      </ClientContextProvider>
+    );
 
     const thirdStageCard = screen.getByText("Prace lakiernicze");
     expect(thirdStageCard).toBeInTheDocument();
   });
 
   it(`should render a fourth stage card with text "Detailing i kontrola jakości"`, () => {
-    render(<OrderStatusPage />);
+    render(
+      <ClientContextProvider>
+        <OrderStatusPage />)
+      </ClientContextProvider>
+    );
 
     const fourthStageCard = screen.getByText("Detailing i kontrola jakości");
     expect(fourthStageCard).toBeInTheDocument();
   });
 
   it(`should render a fifth stage card with text "Zlecenie ukończone. Możesz odebrać auto!"`, () => {
-    render(<OrderStatusPage />);
+    render(
+      <ClientContextProvider>
+        <OrderStatusPage />)
+      </ClientContextProvider>
+    );
 
     const fifthStageCard = screen.getByText(
       "Zlecenie ukończone. Możesz odebrać auto!"
